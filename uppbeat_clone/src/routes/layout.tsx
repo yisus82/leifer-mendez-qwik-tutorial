f6a24016a4e9bd242be3ee7fa5607527934bdbf3
yisus82 <djalma8@gmail.com>
2023-10-30 13:15:26 +0100

    Remove unused parts

--- uppbeat_clone/src/routes/layout.tsx
+++ uppbeat_clone/src/routes/layout.tsx
@@ -1,9 +1,6 @@
 import { component$, Slot, useStyles$ } from "@builder.io/qwik";
-import { routeLoader$ } from "@builder.io/qwik-city";
 import type { RequestHandler } from "@builder.io/qwik-city";
-
-import Header from "~/components/starter/header/header";
-import Footer from "~/components/starter/footer/footer";
+import { routeLoader$ } from "@builder.io/qwik-city";
 
 import styles from "./styles.css?inline";
 
@@ -26,13 +23,5 @@ export const useServerTimeLoader = routeLoader$(() => {
 
 export default component$(() => {
   useStyles$(styles);
-  return (
-    <>
-      <Header />
-      <main>
-        <Slot />
-      </main>
-      <Footer />
-    </>
-  );
+  return (<Slot />);
 });
