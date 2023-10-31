19de2ff38818f0f7db96de82e710a6cb306c6510
yisus82 <djalma8@gmail.com>
2023-10-31 12:13:59 +0100

    Index page

--- uppbeat_clone/src/routes/(common)/layout.tsx
+++ uppbeat_clone/src/routes/(common)/layout.tsx
@@ -1,13 +1,25 @@
 import { Slot, component$ } from '@builder.io/qwik';
 import Header from '~/components/header';
+import Logo from '~/components/logo';
 import Sidebar from '~/components/sidebar';
 
 export default component$(() => {
   return (
-    <>
+    <div class='h-[100vh] flex'>
+    <div class='w-[256px] fixed '>
+      <div class='bg-gray-50 h-[100vh]'>
+        <div class='p-6 border-gray-200 border-b'>
+          <Logo />
+        </div>
+        <div class=''>
+          <Sidebar />
+        </div>
+      </div>
+    </div>
+    <div class='pl-[256px] w-full'>
       <Header />
-      <Sidebar />
       <Slot />
-    </>
+    </div>
+  </div>
   );
 });
