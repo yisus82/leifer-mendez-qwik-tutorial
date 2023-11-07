184b818ccf6494ca71618a9efe3e563e5034d0e1
yisus82 <djalma8@gmail.com>
2023-11-07 11:50:19 +0100

    Make Logo a clickable link

--- uppbeat_clone/src/components/logo/index.tsx
+++ uppbeat_clone/src/components/logo/index.tsx
@@ -1,10 +1,11 @@
 import { component$ } from '@builder.io/qwik';
+import { Link } from '@builder.io/qwik-city';
 import Logo from '~/assets/images/logo.svg';
 
 export default component$(() => {
   return (
-    <span>
+    <Link href='/'>
       <img src={Logo} width="200" height="150" alt="Uppbeat logo"/>
-    </span>
+    </Link>
   );
 });
